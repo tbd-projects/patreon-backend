6d7acfe8acd9c2830fae14aea9bef2d6be96c162
Dmitry Varin <varindv@gmail.com>
2021-09-14 00:24:55 +0300

    add router

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,18 +1,23 @@
 package server
 
 import (
+	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
+	"io"
+	"net/http"
 )
 
 type Server struct {
 	config *Config
 	logger *log.Logger
+	router *mux.Router
 }
 
 func New(config *Config) *Server {
 	return &Server{
 		config: config,
 		logger: log.New(),
+		router: mux.NewRouter(),
 	}
 }
 
@@ -21,10 +26,11 @@ func (s *Server) Start() error {
 	if err := s.configureServer(); err != nil {
 		return err
 	}
+	s.configfureRouter()
 
 	s.logger.Info("starting server")
 
-	return nil
+	return http.ListenAndServe(s.config.BindAddr, s.router)
 }
 
 func (s *Server) configureServer() error {
@@ -36,4 +42,17 @@ func (s *Server) configureServer() error {
 	s.logger.SetLevel(level)
 
 	return nil
+}
+
+func (s *Server) configfureRouter() {
+	s.router.HandleFunc("/hello", s.HandleRoot())
+}
+
+func (s *Server) HandleRoot() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		_, err := io.WriteString(w, "hello patron!")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
