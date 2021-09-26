a15af096ba0679af04746ae1aea3b0f436ed5cf0
Dmitry Varin <varindv@gmail.com>
2021-09-26 21:14:19 +0300

    add log to cors middleware

--- internal/app/sessions/middleware/middleware.go
+++ internal/app/sessions/middleware/middleware.go
@@ -63,6 +63,7 @@ func (m *SessionMiddleware) CheckNotAuthorized(next http.Handler) http.Handler {
 }
 func (m *SessionMiddleware) CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		m.log.Infof("Method: %s Body: %s URL %s", r.Method, r.Body, r.URL)
 		if r.Method == "OPTIONS" {
 			m.log.Info("options request")
 			w.WriteHeader(http.StatusNoContent)
