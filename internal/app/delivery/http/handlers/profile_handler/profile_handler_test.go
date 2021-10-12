345a8dacc4ffd772afce523c16a087859af6638e
Dmitry Varin <varindv@gmail.com>
2021-10-13 01:48:40 +0300

    profile handler fix tests

--- internal/app/delivery/http/handlers/profile_handler/profile_handler_test.go
+++ internal/app/delivery/http/handlers/profile_handler/profile_handler_test.go
@@ -4,93 +4,112 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
 	"patreon/internal/app/delivery/http/handlers"
-	"patreon/internal/app/repository/models"
-	"patreon/internal/app/store"
+	"patreon/internal/app/models"
+	"patreon/internal/app/repository"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
 type ProfileTestSuite struct {
-	handlers.SuiteTestBaseHandler
+	handlers.SuiteHandler
+	handler *ProfileHandler
 }
 
-func (s *ProfileTestSuite) TestServeHTTP_Correct() {
+func (s *ProfileTestSuite) SetupSuite() {
+	s.SuiteHandler.SetupSuite()
+	s.handler = NewProfileHandler(s.Logger, s.MockSessionsManager, s.MockUserUsecase)
+}
+
+func (s *ProfileTestSuite) TestProfileHandler_GET_Correct() {
 	userID := int64(1)
 	test := handlers.TestTable{
-		name:              "correct",
-		data:              &models.User{ID: int(userID), Login: "some", Nickname: "done"},
-		expectedMockTimes: 1,
-		expectedCode:      http.StatusOK,
+		Name:              "correct",
+		Data:              &models.User{ID: userID, Login: "some", Nickname: "done"},
+		ExpectedMockTimes: 1,
+		ExpectedCode:      http.StatusOK,
 	}
 
 	recorder := httptest.NewRecorder()
-	handler := NewProfileHandler(s.logger, s.dataStorage)
 	b := bytes.Buffer{}
-	err := json.NewEncoder(&b).Encode(test.data)
+	err := json.NewEncoder(&b).Encode(s.Tb.Data)
 
 	require.NoError(s.T(), err)
 	ctx := context.WithValue(context.Background(), "user_id", userID)
 	reader, _ := http.NewRequestWithContext(ctx, http.MethodGet, "/user", &b)
 
-	s.mockUserRepository.EXPECT().FindByID(userID).Times(test.expectedMockTimes).Return(test.data.(*models.User), nil)
-	handler.ServeHTTP(recorder, reader)
-	assert.Equal(s.T(), test.expectedCode, recorder.Code)
+	s.MockUserUsecase.
+		EXPECT().
+		GetProfile(userID).
+		Times(test.ExpectedMockTimes).
+		Return(test.Data.(*models.User), nil)
+	s.handler.GET(recorder, reader)
+	assert.Equal(s.T(), test.ExpectedCode, recorder.Code)
 
 	req := &models.Profile{}
 	decoder := json.NewDecoder(recorder.Body)
 	err = decoder.Decode(req)
 	require.NoError(s.T(), err)
 
-	assert.Equal(s.T(), req, &models.Profile{Nickname: test.data.(*models.User).Nickname,
-		Avatar: test.data.(*models.User).Avatar})
+	assert.Equal(s.T(), req, &models.Profile{Nickname: test.Data.(*models.User).Nickname,
+		Avatar: test.Data.(*models.User).Avatar})
 }
 
-func (s *ProfileTestSuite) TestServeHTTP_WitDBError() {
+func (s *ProfileTestSuite) TestProfileHandler_GET_NotFound() {
 	userID := int64(1)
-	test := handlers.TestTable{
-		name:              "with db error",
-		data:              nil,
-		expectedMockTimes: 1,
-		expectedCode:      http.StatusServiceUnavailable,
+	s.Tb = handlers.TestTable{
+		Name:              "with not found",
+		Data:              nil,
+		ExpectedMockTimes: 1,
+		ExpectedCode:      http.StatusNotFound,
 	}
 
 	recorder := httptest.NewRecorder()
-	handler := NewProfileHandler(s.logger, s.dataStorage)
 
 	b := bytes.Buffer{}
-	err := json.NewEncoder(&b).Encode(test.data)
+	err := json.NewEncoder(&b).Encode(s.Tb.Data)
 
 	require.NoError(s.T(), err)
 	ctx := context.WithValue(context.Background(), "user_id", userID)
 	reader, _ := http.NewRequestWithContext(ctx, http.MethodGet, "/user", &b)
 
-	s.mockUserRepository.EXPECT().FindByID(userID).Times(test.expectedMockTimes).Return(nil, store.NotFound)
-	handler.ServeHTTP(recorder, reader)
-	assert.Equal(s.T(), test.expectedCode, recorder.Code)
+	s.MockUserUsecase.EXPECT().
+		GetProfile(userID).
+		Times(s.Tb.ExpectedMockTimes).
+		Return(nil, repository.NotFound)
+	s.handler.GET(recorder, reader)
+	assert.Equal(s.T(), s.Tb.ExpectedCode, recorder.Code)
 }
 
-func (s *ProfileTestSuite) TestServeHTTP_WithoutContext() {
+func (s *ProfileTestSuite) TestProfileHandler_GET_WithoutContext() {
 	userID := int64(1)
-	test := handlers.TestTable{
-		name:              "without context",
-		data:              nil,
-		expectedMockTimes: 0,
-		expectedCode:      http.StatusInternalServerError,
+	s.Tb = handlers.TestTable{
+		Name:              "without context",
+		Data:              nil,
+		ExpectedMockTimes: 0,
+		ExpectedCode:      http.StatusInternalServerError,
 	}
 
 	recorder := httptest.NewRecorder()
-	handler := NewProfileHandler(s.logger, s.dataStorage)
 
 	b := bytes.Buffer{}
-	err := json.NewEncoder(&b).Encode(test.data)
+	err := json.NewEncoder(&b).Encode(s.Tb.Data)
 
 	require.NoError(s.T(), err)
 	reader, _ := http.NewRequest(http.MethodGet, "/user", &b)
 
-	s.mockUserRepository.EXPECT().FindByID(userID).Times(test.expectedMockTimes)
-	handler.ServeHTTP(recorder, reader)
-	assert.Equal(s.T(), test.expectedCode, recorder.Code)
+	s.MockUserUsecase.
+		EXPECT().
+		GetProfile(userID).
+		Times(s.Tb.ExpectedMockTimes)
+	s.handler.GET(recorder, reader)
+	assert.Equal(s.T(), s.Tb.ExpectedCode, recorder.Code)
+}
+func TestProfileHandler(t *testing.T) {
+	suite.Run(t, new(ProfileTestSuite))
 }
