59c1ff7ab5e60cc38ae4d058ae2eb34d025fc21e
Dmitry Varin <varindv@gmail.com>
2021-10-12 20:08:05 +0300

    add tests on user usecase Check()

--- internal/app/usecase/user/user_usecase_test.go
+++ internal/app/usecase/user/user_usecase_test.go
@@ -1,6 +1,7 @@
 package usercase_user
 
 import (
+	models2 "patreon/internal/app/delivery/http/models"
 	"patreon/internal/app/models"
 	"patreon/internal/app/repository"
 	"patreon/internal/app/usecase"
@@ -67,6 +68,96 @@ func (s *SuiteUserUsecase) TestCreatorUsecase_GetProfile_UserFound() {
 	u, err := s.uc.GetProfile(s.Tb.Data.(int64))
 	assert.Equal(s.T(), user, u)
 	assert.Equal(s.T(), s.Tb.ExpectedError, err)
+}
+func (s *SuiteUserUsecase) TestCreatorUsecase_Check_NotFound() {
+	s.Tb = usecase.TestTable{
+		Name:              "User not found",
+		Data:              models.TestUser(),
+		ExpectedMockTimes: 1,
+		ExpectedError:     repository.NotFound,
+	}
+	u := s.Tb.Data.(*models.User)
+	u.Password = "doggy123"
+	req := models2.RequestLogin{
+		Login:    u.Login,
+		Password: u.Password,
+	}
+	s.MockUserRepository.EXPECT().
+		FindByLogin(u.Login).
+		Times(s.Tb.ExpectedMockTimes).
+		Return(nil, repository.NotFound)
+	expectedId := int64(-1)
+	resId, err := s.uc.Check(req.Login, req.Password)
+	assert.Equal(s.T(), expectedId, resId)
+	assert.Equal(s.T(), s.Tb.ExpectedError, errors.Cause(err))
+}
+func (s *SuiteUserUsecase) TestCreatorUsecase_Check_DB_Error() {
+	s.Tb = usecase.TestTable{
+		Name:              "Database error on request",
+		Data:              models.TestUser(),
+		ExpectedMockTimes: 1,
+		ExpectedError:     repository.DefaultErrDB,
+	}
+	u := s.Tb.Data.(*models.User)
+	u.Password = "doggy123"
+	req := models2.RequestLogin{
+		Login:    u.Login,
+		Password: u.Password,
+	}
+	s.MockUserRepository.EXPECT().
+		FindByLogin(u.Login).
+		Times(s.Tb.ExpectedMockTimes).
+		Return(nil, repository.DefaultErrDB)
+	expectedId := int64(-1)
+	resId, err := s.uc.Check(req.Login, req.Password)
+	assert.Equal(s.T(), expectedId, resId)
+	assert.Equal(s.T(), s.Tb.ExpectedError, errors.Cause(err))
+}
+func (s *SuiteUserUsecase) TestCreatorUsecase_Check_InvalidPassword() {
+	s.Tb = usecase.TestTable{
+		Name:              "Invalid password",
+		Data:              models.TestUser(),
+		ExpectedMockTimes: 1,
+		ExpectedError:     IncorrectEmailOrPassword,
+	}
+	u := s.Tb.Data.(*models.User)
+	u.Password = "doggy123"
+	req := models2.RequestLogin{
+		Login:    u.Login,
+		Password: u.Password,
+	}
+	s.MockUserRepository.EXPECT().
+		FindByLogin(u.Login).
+		Times(s.Tb.ExpectedMockTimes).
+		Return(u, nil)
+	expectedId := int64(-1)
+	resId, err := s.uc.Check(req.Login, req.Password)
+	assert.Equal(s.T(), expectedId, resId)
+	assert.Equal(s.T(), s.Tb.ExpectedError, errors.Cause(err))
+}
+func (s *SuiteUserUsecase) TestCreatorUsecase_Check_Correct() {
+	s.Tb = usecase.TestTable{
+		Name:              "User found, password valid",
+		Data:              models.TestUser(),
+		ExpectedMockTimes: 1,
+		ExpectedError:     nil,
+	}
+	u := s.Tb.Data.(*models.User)
+	u.Password = "doggy123"
+	req := models2.RequestLogin{
+		Login:    u.Login,
+		Password: u.Password,
+	}
+	assert.NoError(s.T(), u.Encrypt())
+	s.MockUserRepository.EXPECT().
+		FindByLogin(u.Login).
+		Times(s.Tb.ExpectedMockTimes).
+		Return(u, nil)
+	expectedId := u.ID
+	resId, err := s.uc.Check(req.Login, req.Password)
+	assert.Equal(s.T(), expectedId, resId)
+	assert.Equal(s.T(), s.Tb.ExpectedError, err)
+
 }
 
 func TestUsecaseUser(t *testing.T) {
