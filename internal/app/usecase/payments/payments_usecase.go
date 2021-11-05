b9381119dbf4286a11ae583c25bca08af8de6553
Dmitry Varin <varindv@gmail.com>
2021-11-05 08:43:17 +0300

    implement payments usecase

--- internal/app/usecase/payments/payments_usecase.go
+++ internal/app/usecase/payments/payments_usecase.go
@@ -1,6 +1,9 @@
 package payments
 
-import "patreon/internal/app/repository/payments"
+import (
+	"patreon/internal/app/models"
+	"patreon/internal/app/repository/payments"
+)
 
 type PaymentsUsecase struct {
 	repository payments.Repository
@@ -10,4 +13,30 @@ func NewPaymentsUsecase(repo payments.Repository) *PaymentsUsecase {
 	return &PaymentsUsecase{
 		repository: repo,
 	}
+}
+
+// GetUserPayments Errors:
+//		repository.NotFound
+//		app.GeneralError with Errors:
+//			repository.DefaultErrDB
+func (usecase *PaymentsUsecase) GetUserPayments(userID int64) ([]models.Payments, error) {
+	userPayments, err := usecase.repository.GetUserPayments(userID)
+	if err != nil {
+		return nil, err
+	}
+
+	return userPayments, nil
+}
+
+// GetCreatorPayments Errors:
+//		repository.NotFound
+//		app.GeneralError with Errors:
+//			repository.DefaultErrDB
+func (usecase *PaymentsUsecase) GetCreatorPayments(creatorID int64) ([]models.Payments, error) {
+	creatorPayments, err := usecase.repository.GetCreatorPayments(creatorID)
+	if err != nil {
+		return nil, err
+	}
+
+	return creatorPayments, nil
 }
