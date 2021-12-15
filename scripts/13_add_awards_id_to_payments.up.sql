92cedb09bb4fa09e7e2cb22f23a8b9a43eb8b384
Dmitry Varin <varindv@gmail.com>
2021-12-16 02:10:21 +0300

    fix payments migration (add awards_id)

--- scripts/13_add_awards_id_to_payments.up.sql
+++ scripts/13_add_awards_id_to_payments.up.sql
@@ -1,2 +1,18 @@
 alter table payments
-    add column awards_id bigint not null references awards (awards_id) on delete cascade;
+    add column awards_id bigint references awards (awards_id) on delete cascade;
+
+update payments
+set awards_id = (select distinct awards_id
+                 from awards
+                 where payments.amount = awards.price
+                   and payments.creator_id = awards.creator_id);
+
+delete
+from payments
+where awards_id is null;
+
+alter table payments
+    alter column awards_id set not null;
+
+
+
